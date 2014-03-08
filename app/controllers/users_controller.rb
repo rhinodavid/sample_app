516e8d1866fee23d533b5220db28fd8ede68615d
David Walsh <dawalsh@gmail.com>
2014-03-07 22:06:58 -0500

    Add user editing and admin functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
+
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 30)
+  end
+
   def new
   	@user = User.new
   end
@@ -19,10 +28,52 @@ class UsersController < ApplicationController
 
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updates"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if current_user?(@user)
+      redirect_to root_url
+    else
+      @user.destroy
+      flash[:success] = "User deleted."
+      redirect_to users_url
+    end
+  end
+
 	private
 
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
 		end
+
+    # Before filters
+
+    def signed_in_user
+
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 end
